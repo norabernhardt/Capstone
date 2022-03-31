cffa53886906198c48bc8c97b77f8993952ed17c
Nora B <noraghasel@gmail.com>
2022-03-31 16:33:40 +0200

    Add cloudformation for two s3 buckets and dynamodb

--- stack.sh
+++ stack.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+# CREATE
 aws cloudformation create-stack --stack-name bucketstack --template-body file://create_s3.yml
+# UPDATE
+#aws cloudformation update-stack --stack-name bucketstack --template-body file://create_s3.yml
--- write_to_dynamodb.py
+++ write_to_dynamodb.py
@@ -2,7 +2,7 @@ from urllib import response
 import boto3
 import csv
 
-bucketname="release-service-data-catcher101"
+bucketname="bucket-two-converted-data-2022-04-27"
 
 s3client=boto3.resource("s3")
 s3_object=s3client.Bucket(bucketname).Object("Reihe-22N12.csv").get()
