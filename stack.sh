c178f20e436bdcdaf7cd69300335a1ea88f3d066
Nora B <noraghasel@gmail.com>
2022-04-03 17:48:32 +0200

    Add testfile and shell

--- stack.sh
+++ stack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # CREATE
-aws cloudformation create-stack --stack-name bucketstack --template-body file://create_s3.yml
+aws cloudformation create-stack --stack-name bucketstack --template-body file://cloud_formation.yml
 # UPDATE
-#aws cloudformation update-stack --stack-name bucketstack --template-body file://create_s3.yml
+#aws cloudformation update-stack --stack-name bucketstack --template-body file://cloud_formation.yml
