097f244e5020cb4d1f7e6fc477b69528675f3f1b
Nora B <noraghasel@gmail.com>
2022-04-05 11:48:15 +0200

    Add function to fetch data from S3 and send sorted to dynamodb

--- stack.sh
+++ stack.sh
@@ -3,3 +3,5 @@
 aws cloudformation create-stack --stack-name bucketstack --template-body file://cloud_formation.yml
 # UPDATE
 #aws cloudformation update-stack --stack-name bucketstack --template-body file://cloud_formation.yml
+
+aws s3 cp ./upload_S3/Reihe-22B13.csv s3://bucket-one-raw-data-2022-04-27    
--- write_to_dynamodb.py
+++ write_to_dynamodb.py
@@ -1,10 +1,45 @@
-from urllib import response
 import boto3
-import csv
 
 bucketname="bucket-two-converted-data-2022-04-27"
-
 s3client=boto3.resource("s3")
-s3_object=s3client.Bucket(bucketname).Object("Reihe-22N12.csv").get()
-text=s3_object["Body"].read()
-print(text)
+dynamodb=boto3.resource("dynamodb") 
+
+def write_to_dynamo(csv_content):
+    table=dynamodb.Table("tables-for-preferences")
+    lines=csv_content.splitlines()
+    for line in lines:
+        content=line.split("\t")
+        title=content[0]
+        author=content[2]
+        publisher=content[3]
+        place=content[4]
+        year=content[5]
+        isbn=content[7]
+        edition=content[8]
+        pages=content[10]
+        ddc=content[19]
+        keywords=content[20]
+        table_url=content[22]
+        summary_url=content[23]
+        
+        table.put_item(Item={
+            'Titel': title,
+            'Verfasser': author,
+            'Verlag': publisher,
+            'Verlagsort': place,
+            'Erscheinungsjahr': year,
+            'ISBN': isbn,
+            'Auflage': edition,
+            'Seiten': pages,
+            'DDC-Sachgruppe': ddc,
+            'Schlagworte': keywords,
+            'Inhaltsverzeichnis': table_url,
+            'Inhaltstext': summary_url
+            }
+        )       
+
+bucket=s3client.Bucket(bucketname)
+for obj in bucket.objects.all():
+    key=obj.key
+    body=obj.get()["Body"].read()
+    write_to_dynamo(body.decode("utf-8"))
